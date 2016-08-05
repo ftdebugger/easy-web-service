0c5446f37c0684269a1f629b041434162579deb8
next0 <next0@yandex-team.ru>
2016-08-05 20:33:28 +0300

    add travis ci integration

---
+++ bin/publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ev
+
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
+    docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+    docker push "$DOCKER_IMAGE_NAME";
+fi
